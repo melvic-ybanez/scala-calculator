e9cfe0afc05193119871ad884a99f1752514b818
Melvic Ybanez <ybamelcash@yahoo.com>
2013-06-13 21:33:42 +0800

    Update Math.scala

--- src/calculator/model/Math.scala
+++ src/calculator/model/Math.scala
@@ -25,28 +25,17 @@ object Math {
     case '(' | ')' => 2
   }
   
-  def factorial(n: Int): Int = if (n < 1) 1 else n * factorial(n - 1)
-  
-  def permutation(n: Int, r: Int) = factorial(n) / factorial(n - r)
-  
-  def combination(n: Int, r: Int) = factorial(n) / factorial(r) * factorial(n - r)
-  
-  def isWholeNumber(n: Double) = n - n.toInt == 0
-  
-  def formatNum(n: Double) = if (isWholeNumber(n)) n.toInt.toString else n.toString
-  
-  def binOps = "^*/+-" + specialBinOps
-  
-  def unOps = preUnOps + "!"
-  
-  def preUnOps = specialUnOps + "("
-  
-  def isSpecialUnOp(s: String) = specialUnOps.contains(s)
-  
-  def isSpecialBinOp(s: String) = specialBinOps.contains(s)
-  
-  private def specialUnOps = s"$sqrt$log$ln$sin$cos$tan$sinh$cosh$tanh"
-  
+  def factorial(n: Int): Int = if (n < 1) 1 else n * factorial(n - 1)  
+  def permutation(n: Int, r: Int) = factorial(n) / factorial(n - r)  
+  def combination(n: Int, r: Int) = factorial(n) / (factorial(r) * factorial(n - r))  
+  def isWholeNumber(n: Double) = n - n.toInt == 0  
+  def formatNum(n: Double) = if (isWholeNumber(n)) n.toInt.toString else n.toString  
+  def binOps = "^*/+-" + specialBinOps  
+  def unOps = preUnOps + "!"  
+  def preUnOps = specialUnOps + "("  
+  def isSpecialUnOp(s: String) = specialUnOps.contains(s)  
+  def isSpecialBinOp(s: String) = specialBinOps.contains(s)  
+  private def specialUnOps = s"$sqrt$log$ln$sin$cos$tan$sinh$cosh$tanh"  
   private def specialBinOps = s"$perm$comb"
   
   val sqrt = "\u221A"
@@ -56,8 +45,8 @@ object Math {
   val cos = "c"
   val tan = "t"
   val sinh = "a"
-  val cosh = "b"
-  val tanh = "c"
+  val cosh = "y"
+  val tanh = "z"
   val perm = "P"
   val comb = "C"
 }
