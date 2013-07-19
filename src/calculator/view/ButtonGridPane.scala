35a942333999ba72a06ee4daf9f7e2b6863a6b74
Melvic Ybanez <ybamelcash@yahoo.com>
2013-07-19 22:33:46 +0800

    Update ButtonGridPane.scala

--- src/calculator/view/ButtonGridPane.scala
+++ src/calculator/view/ButtonGridPane.scala
@@ -1,6 +1,8 @@
 package calculator.view
 
 import swing._
+import java.awt.Font
+import scala.swing.Font
 
 class ButtonGridPane extends BorderPanel {
   class ButtonGridPane extends GridBagPanel {
@@ -13,6 +15,7 @@ class ButtonGridPane extends BorderPanel {
       val button = new Button {
         text = str
         preferredSize = new Dimension(55, 30)
+        font = new Font("Tahoma", Font.PLAIN, 12)
       }
 
       c.gridx = x
@@ -22,6 +25,7 @@ class ButtonGridPane extends BorderPanel {
       button
     }
   }
+  
   val northPane = new ButtonGridPane {
     val sinButton = createButton("sin", 0, 0)
     val cosButton = createButton("cos", 1, 0)
@@ -36,23 +40,24 @@ class ButtonGridPane extends BorderPanel {
 
     border = Swing.EmptyBorder(0, 0, 9, 0)
   }
+  
   val centerPane = new ButtonGridPane {
     val sevenButton = createButton("7", 0, 0)
     val eightButton = createButton("8", 1, 0)
     val nineButton = createButton("9", 2, 0)
     val multiplyButton = createButton("x", 3, 0)
-    val divideButton = createButton("/", 4, 0)
+    val divideButton = createButton("\u00f7", 4, 0)
     val rootButton = createButton("\u221A", 5, 0)
     val fourButton = createButton("4", 0, 1)
     val fiveButton = createButton("5", 1, 1)
     val sixButton = createButton("6", 2, 1)
     val addButton = createButton("+", 3, 1)
     val subButton = createButton("-", 4, 1)
-    val factButton = createButton("!", 5, 1)
+    val factButton = createButton("n!", 5, 1)
     val oneButton = createButton("1", 0, 2)
     val twoButton = createButton("2", 1, 2)
     val threeButton = createButton("3", 2, 2)
-    val expButton = createButton("^", 3, 2)
+    val expButton = createButton("x\u207f", 3, 2)
     val paren1Button = createButton("(", 4, 2)
     val paren2Button = createButton(")", 5, 2)
     val zeroButton = createButton("0", 0, 3)
@@ -61,9 +66,9 @@ class ButtonGridPane extends BorderPanel {
     val msButton = createButton("MS", 3, 3)
     val mrButton = createButton("MR", 4, 3)
     val mcButton = createButton("MC", 5, 3)
-    val ansButton = createButton("ANS", 0, 4, 2)
-    val bsButton = createButton("BS", 2, 4, 2)
-    val clButton = createButton("CL", 4, 4, 2)
+    val ansButton = createButton("Answer", 0, 4, 2)
+    val bsButton = createButton("Backspace", 2, 4, 2)
+    val clButton = createButton("Clear", 4, 4, 2)
 
     border = Swing.EmptyBorder(9, 0, 0, 0)
   }
