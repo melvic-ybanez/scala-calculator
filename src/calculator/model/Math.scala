b0bf9f951de180318c4c9322ceb0c82418f9d3e0
Melvic Ybanez <ybamelcash@yahoo.com>
2013-07-19 22:31:04 +0800

    Update Math.scala

--- src/calculator/model/Math.scala
+++ src/calculator/model/Math.scala
@@ -30,12 +30,12 @@ object Math {
   def combination(n: Int, r: Int) = factorial(n) / (factorial(r) * factorial(n - r))  
   def isWholeNumber(n: Double) = n - n.toInt == 0  
   def formatNum(n: Double) = if (isWholeNumber(n)) n.toInt.toString else n.toString  
-  def binOps = "^*/+-" + specialBinOps  
+  def binOps = "^*/+-" + specialBinOps
   def unOps = preUnOps + "!"  
   def preUnOps = specialUnOps + "("  
   def isSpecialUnOp(s: String) = specialUnOps.contains(s)  
   def isSpecialBinOp(s: String) = specialBinOps.contains(s)  
-  private def specialUnOps = s"$sqrt$log$ln$sin$cos$tan$sinh$cosh$tanh"  
+  private def specialUnOps = s"$sqrt$log$ln$sin$cos$tan$sinh$cosh$tanh"
   private def specialBinOps = s"$perm$comb"
   
   val sqrt = "\u221A"
