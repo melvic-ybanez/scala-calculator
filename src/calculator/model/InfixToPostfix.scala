bbcab01110c8f410c0eec602362f84d479321beb
Melvic Ybanez <ybamelcash@yahoo.com>
2013-07-19 22:30:22 +0800

    Update InfixToPostfix.scala

--- src/calculator/model/InfixToPostfix.scala
+++ src/calculator/model/InfixToPostfix.scala
@@ -1,13 +1,15 @@
 package calculator.model
 
+import Math._
+
 class InfixToPostfix private(expression: String) {
   def toPostfix = {        
     def normalized(norm: String, exp: String): String = 
       if (exp.isEmpty) norm
       else if (norm.isEmpty) normalized(norm + exp.head, exp.tail)
       else (norm.last, exp.head) match {
-        case (nl, eh) if nl.isDigit && Math.isPreUnOp(eh) => normalized(s"$norm*$eh", exp.tail)
-        case ('-', eh) if Math.isPreUnOp(eh) => normalized(s"${norm}1*$eh", exp.tail)
+        case (nl, eh) if nl.isDigit && isPreUnOp(eh) => normalized(s"$norm*$eh", exp.tail)
+        case ('-', eh) if isPreUnOp(eh) => normalized(s"${norm}1*$eh", exp.tail)
         case ('+', '-') | ('-', '+') => normalized(norm.init + "-", exp.tail)
         case (nl, eh) => normalized(norm + eh, exp.tail)
       }
@@ -18,7 +20,7 @@ class InfixToPostfix private(expression: String) {
       else (tokens.head, exp.head) match {
         case (th, eh) if (eh.isDigit || eh == '.') && (th.last.isDigit || th.last == '.') => 
           tokenized((th + eh) :: tokens.tail, exp.tail)
-        case (th, eh) if Math.isOperator(th.head) && (eh == '+' || eh == '-') =>
+        case (th, eh) if isOperator(th.head) && (eh == '+' || eh == '-') =>
           tokenized((eh + "0") :: tokens, exp.tail)
         case (th, eh) => tokenized(eh.toString :: tokens, exp.tail)
       }    
@@ -32,18 +34,17 @@ class InfixToPostfix private(expression: String) {
         case eh :: et => ops match {
           case Nil => convert(et, postfix, List(eh.head))
           case oh :: ot =>
-            if (Math.precedence(eh.head) > Math.precedence(oh))
+            if (precedence(eh.head) > Math.precedence(oh))
               convert(et, postfix, eh.head :: ops)
             else convert(exp, oh.toString :: postfix, ot)
         }
       }
    
-    val newExp = expression.replace("log ", Math.log).replace("ln ", Math.ln)
-      .replace("sin ", Math.sin).replace("cos ", Math.cos)
-      .replace("tan ", Math.tan).replace("sinh ", Math.sinh)
-      .replace("cosh ", Math.cosh).replace("tanh ", Math.tanh)
-      .replace("P", Math.perm).replace("C", Math.comb)
-      .replace("x", "*").replace("E", "*10^")
+    val newExp = expression.replace("log ", log).replace("ln ", ln)
+      .replace("sin ", sin).replace("cos ", cos).replace("tan ", tan)
+      .replace("sinh ", sinh).replace("cosh ", cosh).replace("tanh ", tanh)
+      .replace("P", perm).replace("C", comb).replace("x", "*")
+      .replace("\u00f7", "/").replace("E", "*10^")
     val tokens = tokenized(Nil, normalized("", "0+" + newExp)).reverse
     val (valid, msg) = Validator(tokens)
     (valid, if (valid) convert(tokens, Nil, Nil) else tokens, msg)
